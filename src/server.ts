e127cf84148138cdc172c7a7006f0ea3cb7beea2
luise-go <luise.goede@yahoo.de>
2021-06-10 16:21:03 +0200

    :bug: Fix node modules bug

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
+import path from 'path';
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const { PORT = 3000 } = process.env;
 
@@ -9,6 +12,11 @@ app.use('/storybook', express.static('dist/storybook'));
 
 // Serve app production bundle
 app.use(express.static('dist/app'));
+
+// Handle client routing, return all requests to the app
+app.get('*', (_req: express.Response, res: express.Request) => {
+  res.sendFile(path.join(__dirname, 'app/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`photoplay app listening at http://localhost:${PORT}`);
