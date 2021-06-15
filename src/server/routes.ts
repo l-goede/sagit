a2ec8766552d01598065f131eb95e4556133e3a5
luise-go <luise.goede@yahoo.de>
2021-06-15 17:30:39 +0200

    :boom: Web scraping via cheerio and add headers for getting data

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import fetch from 'node-fetch';
-import domParser from 'dom-parser';
+import cheerio from 'cheerio';
 
 const router = express.Router();
 
@@ -17,14 +17,16 @@ export default router;
 
 async function searchProducts(name: string) {
   const response = await fetch(
-    `https://www.idealo.de/preisvergleich/MainSearchProductCategory.html?q=${name}`
+    `https://www.idealo.de/preisvergleich/MainSearchProductCategory.html?q=${name}`,
+    {
+      headers: {
+        'accept-language': 'de-DE',
+      },
+    }
   );
   const rawHtml = await response.text();
+  const $ = cheerio.load(rawHtml);
+  const product = $('.offerList-item-priceMin').text();
 
-  const parser = new domParser();
-  const dom = parser.parseFromString(rawHtml);
-  const price = dom.getElementsByClassName('myofferList-item-priceMin');
-  const product = { name: name, producer: 'tba', price: price };
-  const products = [product];
-  return products;
+  console.log(product);
 }
