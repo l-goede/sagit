47e0b0486e7c44ad4524d08f92f809ad1f0d0332
luise-go <luise.goede@yahoo.de>
2021-06-16 17:17:04 +0200

    :sparkles: Add readCredentials and saveCredential to server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 import path from 'path';
 import router from './server/routes';
-import { connectDatabase, disconnectDatabase } from './utils/database';
+import { connectDatabase } from './utils/database';
+import { readCredentials, saveCredential } from './utils/credentials';
 
 const { PORT = 3331 } = process.env;
 
 console.log(process.env.MONGO_URL);
 
 const app = express();
+
+app.use(express.json());
 
 app.use('/api', router);
 
@@ -24,17 +27,27 @@ app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, 'app/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`app listening at http://localhost:${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`sagit listening at http://localhost:${PORT}`);
+// });
+
+if (process.env.MONGODB_URL === undefined) {
+  throw new Error('Missing env MONGODB_URL');
+}
+
+app.get('/api/credentials', async (_request, response) => {
+  const credentials = await readCredentials();
+  response.json(credentials);
 });
 
-const start = async () => {
-  if (process.env.MONGODB_URL === undefined) {
-    throw new Error('Missing env MONGODB_URL');
-  }
-  await connectDatabase(process.env.MONGODB_URL);
+app.post('/api/credentials', async (request, response) => {
+  await saveCredential(request.body);
+  response.send('Credential saved in db');
+});
 
-  await disconnectDatabase();
-};
-
-start();
+connectDatabase(process.env.MONGODB_URL).then(() => {
+  console.log('Database connected');
+  app.listen(PORT, () => {
+    console.log(`Sagit listening at http://localhost:${PORT}`);
+  });
+});
