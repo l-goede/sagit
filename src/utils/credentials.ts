c176309f76a02f07a64f5777855b79dffcc1ba4f
luise-go <luise.goede@yahoo.de>
2021-06-16 16:58:18 +0200

    :sparkles: Add saveCredential function

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -4,3 +4,9 @@ import { getCredentialsCollection } from './database';
 export const readCredentials = async (): Promise<Credential[]> => {
   return await getCredentialsCollection().find().sort({ service: 1 }).toArray();
 };
+
+export const saveCredential = async (credential: Credential): Promise<void> => {
+  await getCredentialsCollection().insertOne({
+    ...credential,
+  });
+};
