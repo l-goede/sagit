3e7246efc2855be8664efcbef4c7fab418bc83eb
luise-go <luise.goede@yahoo.de>
2021-06-16 14:33:34 +0200

    :sparkles: Connect client

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,3 +1,12 @@
+import { MongoClient } from 'mongodb';
+
 export const connectDatabase = async (url: string): Promise<void> => {
-  console.log(url);
+  const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  await client.connect();
+  const databaseList = await client.db().admin().listDatabases();
+  console.log(databaseList);
 };
