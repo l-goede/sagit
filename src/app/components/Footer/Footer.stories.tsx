19445d267c1fe4274ce3dadba37432c8a00da854
luise-go <luise.goede@yahoo.de>
2021-06-22 16:42:14 +0200

    Temporary rename components folder

--- src/app/components/Footer/Footer.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Footer from './Footer';
-
-export default {
-  title: 'Component/Footer',
-  component: Footer,
-};
-
-export const NavBar = (): JSX.Element => <Footer />;
--- src/app/components/SampleButton/SampleButton.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Button from './SampleButton';
-
-test('renders Start Button', () => {
-  render(<Button>Start</Button>);
-  expect(screen.getByRole('button').innerHTML).toBe('Start');
-});
