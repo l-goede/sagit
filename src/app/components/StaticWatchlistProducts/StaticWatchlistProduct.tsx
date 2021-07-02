8908efb873f3793090c1101dcaea8f85b1ebdf70
luise-go <luise.goede@yahoo.de>
2021-07-03 00:13:10 +0200

    💄 Adjust style of static watchlist component

--- src/app/components/StaticWatchlistProducts/StaticWatchlistProduct.tsx
+++ src/app/components/StaticWatchlistProducts/StaticWatchlistProduct.tsx
@@ -5,35 +5,32 @@ import styles from './StaticWatchlistProduct.module.css';
 
 function StaticWatchlistProduct(): JSX.Element {
   return (
-    <div className={styles.staticWatchlistProduct}>
+    <article className={styles.staticWatchlistProduct}>
       <img
         className={styles.staticWatchlistProduct__img}
         src="https://cdn.idealo.com/folder/Product/6503/2/6503294/s1_produktbild_max/bang-olufsen-beoplay-e8-2-0-black.jpg"
       />
-      <div className={styles.staticWatchlistProduct__card}>
-        <div className={styles.staticWatchlistProduct__titleWrapper}>
-          <p className={styles.staticWatchlistProduct__title}>
-            Bang & Olufsen BeoPlay E8
-          </p>
-          <button className={styles.staticWatchlistProduct__icon}>
-            <SavedIcon />
-          </button>
-        </div>
+      <div className={styles.staticWatchlistProduct__text}>
+        <p className={styles.staticWatchlistProduct__title}>
+          Bang & Olufsen BeoPlay E8
+        </p>
+
         <p className={styles.staticWatchlistProduct__description}>
           Bluetooth-Kopfhörer im Ohr sitzend kabellos Bluetooth 4.2 Frequenzgang
           20 - 20.000 Hz
         </p>
         <p className={styles.staticWatchlistProduct__price}>ab 81,95 €</p>
-        <div className={styles.staticwatchlistProduct__targetPriceWrapper}>
-          <p className={styles.staticWatchlistProduct__targetPrice}>
-            Wunschpreis: 70 €
-          </p>
-          <button className={styles.staticwatchlistProduct__shoplink}>
-            <ShopLink />
-          </button>
-        </div>
+        <p className={styles.staticWatchlistProduct__targetPrice}>
+          Wunschpreis: 70 €
+        </p>
+        <button className={styles.staticwatchlistProduct__shoplink}>
+          <ShopLink />
+        </button>
       </div>
-    </div>
+      <button className={styles.staticWatchlistProduct__icon}>
+        <SavedIcon />
+      </button>
+    </article>
   );
 }
 export default StaticWatchlistProduct;
