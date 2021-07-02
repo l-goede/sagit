0265e3d81cb624c13d9f7fb7659eb430fa60ab1e
luise-go <luise.goede@yahoo.de>
2021-07-02 13:58:08 +0200

    ✨ Add shop link button to watchlist

--- src/app/components/ShopLink/ShopLink.tsx
+++ src/app/components/ShopLink/ShopLink.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import ShopLinkIcon from '../Icons/ShopLinkIcon';
 
 type ShopLinkProps = {
   icon: JSX.Element;
   link: string;
 };
-function ShopLink({ icon, link }: ShopLinkProps): JSX.Element {
-  return <a href={link}>{icon}</a>;
+function ShopLink({ link }: ShopLinkProps): JSX.Element {
+  return (
+    <a href={link}>
+      <ShopLinkIcon />
+    </a>
+  );
 }
 
 export default ShopLink;
