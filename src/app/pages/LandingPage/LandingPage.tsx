3b2e6426569357e9d345d16779e0cbe8cf9a9c91
luise-go <luise.goede@yahoo.de>
2021-07-03 00:56:37 +0200

    ✨ Add use useHistory hook for navigate to search page

--- src/app/pages/LandingPage/LandingPage.tsx
+++ src/app/pages/LandingPage/LandingPage.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import StartLogoIcon from '../../components/Icons/StartLogoIcon';
 import styles from './LandingPage.module.css';
 
 function LandingPage(): JSX.Element {
+  const history = useHistory();
+  history.push('/search');
   return (
     <div className={styles.landingPage}>
       <StartLogoIcon />
