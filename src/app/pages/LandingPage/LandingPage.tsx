8d69c58c533532c7e52cb204545f706b711ee60d
luise-go <luise.goede@yahoo.de>
2021-07-03 00:55:09 +0200

    Animate logo for landing page

--- src/app/pages/LandingPage/LandingPage.tsx
+++ src/app/pages/LandingPage/LandingPage.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import LogoIcon from '../../components/Icons/LogoIcon';
+import StartLogoIcon from '../../components/Icons/StartLogoIcon';
 import styles from './LandingPage.module.css';
 
 function LandingPage(): JSX.Element {
   return (
     <div className={styles.landingPage}>
-      <LogoIcon />
+      <StartLogoIcon />
     </div>
   );
 }
